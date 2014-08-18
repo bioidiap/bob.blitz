4647efcb590d1ba6cbddd3b81c9b2c328873c67d
Manuel Guenther <manuel.guenther@idiap.ch>
2014-08-18 09:38:46 +0200

    Extended bob.extension.Library with blitz includes.

--- bob/blitz/extension.py
+++ bob/blitz/extension.py
@@ -10,7 +10,7 @@ import numpy
 from pkg_resources import resource_filename
 from bob.extension import Extension as BobExtension
 # forward the build_ext command from bob.extension
-from bob.extension import Library, build_ext
+from bob.extension import build_ext, Library as BobLibrary
 from distutils.version import StrictVersion
 
 class Extension(BobExtension):
@@ -52,3 +52,44 @@ class Extension(BobExtension):
 
     # Run the constructor for the base class
     BobExtension.__init__(self, *args, **kwargs)
+
+
+class Library (BobLibrary):
+  """Pure C++ library builing with blitz array.
+
+  See the documentation for :py:class:`bob.extension.Extension` for more
+  details on input parameters.
+  """
+
+  def __init__(self, *args, **kwargs):
+    """Initialize the library with parameters.
+
+    This library adds ``blitz>=0.10`` as a requirement for library derived
+    from this class.
+
+    See the help for :py:class:`bob.extension.Library` for more details on
+    options.
+    """
+
+    require = ['blitz>=0.10', 'boost']
+
+    kwargs.setdefault('packages', []).extend(require)
+
+    self_include_dir = resource_filename(__name__, 'include')
+    kwargs.setdefault('include_dirs', []).append(numpy.get_include())
+    kwargs.setdefault('include_dirs', []).append(self_include_dir)
+
+    # TODO: are these macros required for pure C++ builds?
+    macros = [
+          ("PY_ARRAY_UNIQUE_SYMBOL", "BOB_NUMPY_C_API"),
+          ("NO_IMPORT_ARRAY", "1"),
+          ]
+
+    if StrictVersion(numpy.__version__) >= StrictVersion('1.7'):
+      macros.append(("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION"))
+
+    kwargs.setdefault('define_macros', []).extend(macros)
+
+    # Run the constructor for the base class
+    BobLibrary.__init__(self, *args, **kwargs)
+
