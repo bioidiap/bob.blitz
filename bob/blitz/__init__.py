32bb9980ab66002e90cf5fe41151e96e9fb8206a
Manuel Guenther <manuel.guenther@idiap.ch>
2015-04-23 13:35:44 +0200

    Switched to use the new bob.extension.get_config function

--- bob/blitz/__init__.py
+++ bob/blitz/__init__.py
@@ -14,20 +14,8 @@ def get_config():
   """Returns a string containing the configuration information.
   """
 
-  from .version import externals
-
-  packages = pkg_resources.require(__name__)
-  this = packages[0]
-  deps = packages[1:]
-
-  retval =  "%s: %s [api=0x%04x] (%s)\n" % (this.key, this.version,
-      version.api, this.location)
-  retval += "  - c/c++ dependencies:\n"
-  for k in sorted(externals): retval += "    - %s: %s\n" % (k, externals[k])
-  retval += "  - python dependencies:\n"
-  for d in deps: retval += "    - %s: %s (%s)\n" % (d.key, d.version, d.location)
-
-  return retval.strip()
+  import bob.extension
+  return bob.extension.get_config(__name__, version.externals, version.api)
 
 # gets sphinx autodoc done right - don't remove it
 __all__ = [_ for _ in dir() if not _.startswith('_')]
