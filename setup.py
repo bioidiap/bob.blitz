faf413a1ce7bda83e7cc192eb12dab41e9858d52
Pavel Korshunov <pavel.korshunov@idiap.ch>
2015-11-19 15:38:27 +0100

    pkg_resources is replaces with pkgutil

--- setup.py
+++ setup.py
@@ -55,9 +55,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
 
-    namespace_packages = [
-      'bob',
-    ],
+
 
     setup_requires = build_requires,
     install_requires = build_requires,
